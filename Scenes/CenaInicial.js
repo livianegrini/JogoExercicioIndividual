8e11f0f90e764e7dcf9c7bd6f214fcc1f724b383
livianegrini <livia.negrini@sou.inteli.edu.br>
2025-03-07 19:46:16 -0300

    Projeto Individual mudando rotas imagens

--- Scenes/CenaInicial.js
+++ Scenes/CenaInicial.js
@@ -8,9 +8,9 @@ class CenaInicial extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image("capa", "../assets/capa.png");
-        this.load.image("botaoJogar", "../assets/botaoJogar.png");
-        this.load.image("botaoComoJogar", "../assets/botaoComoJogar.png");
+        this.load.image("capa", "Assets/capa.png");
+        this.load.image("botaoJogar", "Assets/botaoJogar.png");
+        this.load.image("botaoComoJogar", "Assets/botaoComoJogar.png");
     }
 
     create() {
